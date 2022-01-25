d77f48057761e4d4c9867a1d3c6a9f4e2b6751b4
gustav042 <gustavofelipereira42@gmail.com>
2022-01-25 15:10:36 -0300

    triangulo

--- t3.c
+++ t3.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int tipo_triangulo(float x, float y, float z};
+int tipo_triangulo(float x, float y, float z);
 
 int main(){
 	printf("feijao");
 	printf("farofa");
 	return 0;}
+
+int tipo_tringulo(float x, float y, float z){
+	printf
+}
