1671dd582c716d47d5743cf28607b78273cda3b2
gustav042 <gustavofelipereira42@gmail.com>
2022-01-25 16:15:47 -0300

    conflict

--- t3.c
+++ t3.c
@@ -5,7 +5,7 @@ int tipo_triangulo(float x, float y, float z);
 
 int main(){
 	printf("feijao");
-	printf("farofa");
+	printf("feijoada");
 	return 0;}
 
 int tipo_tringulo(float x, float y, float z){
