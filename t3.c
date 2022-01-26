25465dd9d0eee0ee35fd777680350af6b360fc03
gustav042 <92122270+gustav042@users.noreply.github.com>
2022-01-26 09:45:53 -0300

    Pra testar trabalhar em equipe

--- t3.c
+++ t3.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
-<<<<<<< HEAD
-
-int main(){
-	printf("feijao");
-=======
 #include <stdlib.h>
-
 int tipo_triangulo(float x, float y, float z);
 
 int main(){
 	printf("feijao");
 	printf("farofa");
->>>>>>> refs/remotes/origin/main
 	return 0;}
 
 int tipo_tringulo(float x, float y, float z){
-	printf
+	printf("Aff");
 }
